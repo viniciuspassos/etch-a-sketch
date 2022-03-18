ec2f39dc3770db3c931bbb4fa6bb1fc2ebbf1b5b
Vinicius Passos <passos.vinicius01@gmail.com>
2022-03-17 21:52:50 -0300

    get user input

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const grid = document.querySelector(".grid-container")
-for (i = 0; i < 32; i++) {
-    const square = document.createElement("div")
-    square.className = 'grid-item';
-    grid.appendChild(square)
+const grid = document.querySelector(".grid-container");
+createGrid();
+
+const clearButton = document.querySelector("button");
+clearButton.addEventListener("click", function (event) {
+  const squares = document.querySelectorAll(".grid-item");
+  squares.forEach((item) => item.remove());
+  const newSize = window.prompt("Grid size. Max 100");
+  const isValid = validateSize(newSize);
+  if (!isValid) {
+    alert("Size to big");
+    return;
+  }
+  createGrid(newSize * 2);
+});
+
+const validateSize = (userSize) => {
+  if (userSize > 100) {
+    return false;
+  }
+  return true;
+};
+
+function createGrid(size = 32) {
+  for (i = 0; i < size; i++) {
+    const square = document.createElement("div");
+    square.className = "grid-item";
+    grid.appendChild(square);
+
+    square.addEventListener("mouseover", function (event) {
+      square.style.backgroundColor = "green";
+    });
+  }
 }
